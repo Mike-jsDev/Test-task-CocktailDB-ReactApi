efd59acb009563d26615ee307d2cee609be5be1e
Mikhail-it <52753759+Mikhail-it@users.noreply.github.com>
2020-03-04 21:14:16 +0300

    commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,8 @@
 import {applyMiddleware, createStore} from 'redux'
-import {composeEnhancers} from './utils'
 import * as fromRoot from './index.reducer'
-import {createLogger} from 'redux-logger'
 import thunk from 'redux-thunk'
 
-function configureStore() {
-    const getLogger = () => {
-        return createLogger({collapsed: true})
-    }
 
-    const enhancer = composeEnhancers(
-        applyMiddleware(
-            thunk,
-            getLogger()
-        )
-    )
-    return createStore(fromRoot.reducers, {}, enhancer)
-}
 
-const store = configureStore()
+const store = createStore(fromRoot.reducers, applyMiddleware(thunk))
 export default store;
